c870805c1008c7a4109ed9504f5f3a4524191221
prince475 <danielmwai475@gmail.com>
2023-03-25 15:56:47 +0300

    Passing javascript inside our jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const firstName = "Joe"
+  const lastName = "Schmoe"
+  const  date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+    if (hours < 12){
+    timeOfDay = "morning"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+    }else if (hours >= 17 && hours < 19) {
+      timeOfDay = "evening"
+    }else {
+      timeOfDay = "night"
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello {firstName} {lastName}!</h1>
+      <h2>It is currently about {hours} o'clock</h2>
+      <h2>Good {timeOfDay}!</h2>
     </div>
-  );
+  )
 }
 
 export default App;
